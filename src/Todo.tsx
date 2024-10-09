69ba038dad78706dc7bcc78bed06476819a74885
manalBatta <95074111+manalBatta@users.noreply.github.com>
2024-10-09 13:13:49 +0300

    display all todos

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,5 +1,7 @@
 import {
   Box,
+  Grid2,
+  IconButton,
   ListItemButton,
   ListItemIcon,
   ListItemText,
@@ -8,14 +10,63 @@ import {
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Checkbox from "@mui/material/Checkbox";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useId } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
 type TodoItem = {
+  id: string;
   text: string;
   complete: boolean;
 };
 const Todo = () => {
-  const [todoList, setTodoList] = useState<TodoItem[] | null>(null);
+  const initialState = [
+    {
+      id: useId(),
+      text: "Buy groceries",
+      complete: false,
+    },
+    {
+      id: useId(),
+      text: "Finish React project",
+      complete: true,
+    },
+    {
+      id: useId(),
+      text: "Schedule doctor appointment",
+      complete: false,
+    },
+    {
+      id: useId(),
+      text: "Walk the dog",
+      complete: true,
+    },
+    {
+      id: useId(),
+      text: "Read a book for 30 minutes",
+      complete: false,
+    },
+  ];
+
+  const [todoList, setTodoList] = useState<TodoItem[] | null>(initialState);
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    id: string
+  ): void => {
+    setTodoList((prev) => {
+      if (!prev) return null;
+
+      return prev.map((item) => {
+        if (item.id === id) {
+          return { ...item, complete: event.target.checked };
+        }
+        return item;
+      });
+    });
+  };
 
   return (
     <>
@@ -23,40 +74,30 @@ const Todo = () => {
         <Typography sx={{ mb: 2, p: 2 }} variant="h3" component="div">
           My To-Do List
         </Typography>
+        <Grid2>
+          <TextField label="freeSolo" />
+          <IconButton>
+            <AddIcon></AddIcon>
+          </IconButton>
+        </Grid2>
+
         <List>
-          <ListItem sx={{ background: "rgb(207, 255, 167)" }} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <Checkbox
-                  onChange={(event) =>
-                    console.log(event.target.checked)
-                  }></Checkbox>
-              </ListItemIcon>
-              <ListItemText primary="ToDo today is nothing to do" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <Checkbox
-                  onChange={(event) =>
-                    console.log(event.target.checked)
-                  }></Checkbox>
-              </ListItemIcon>
-              <ListItemText primary="ToDo today is nothing to do" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <Checkbox
-                  onChange={(event) =>
-                    console.log(event.target.checked)
-                  }></Checkbox>
-              </ListItemIcon>
-              <ListItemText primary="ToDo today is nothing to do" />
-            </ListItemButton>
-          </ListItem>
+          {todoList?.length &&
+            todoList.map((todo: TodoItem) => {
+              return (
+                <ListItem disablePadding key={todo.id}>
+                  {/* TODO: replce the listItemButton with appropriate component */}
+                  <ListItemButton>
+                    <Checkbox
+                      checked={todo.complete}
+                      onChange={(event) =>
+                        handleChange(event, todo.id)
+                      }></Checkbox>
+                    <ListItemText primary={todo.text} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
         </List>
       </Box>
     </>
